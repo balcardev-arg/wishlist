9274763dd5aa38ad14ed90883cd18db511df1ab2
layla <cisneroslay@hotmail.com>
2023-01-16 11:20:11 -0300

    initial commit

---
+++ WishList/WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Layla Cisneros on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
