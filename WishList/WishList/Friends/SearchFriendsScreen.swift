c4986cacf41eaa5db6cce219040499ddfd939942
layla <cisneroslay@hotmail.com>
2023-01-25 11:44:39 -0300

    Search people initial message and empty list UI

--- WishList/WishList/Friends/SearchFriendsScreen.swift
+++ WishList/WishList/Friends/SearchFriendsScreen.swift
@@ -10,23 +10,48 @@ import SwiftUI
 struct SearchFriendsScreen: View {
     
     @State private var searchText : String = ""
+    @State private var showInitialMessage = true
     
-    private let fullFakeFriends: [FakeFriend] = [
-        FakeFriend(id: "1", name: "Gian El hombre", urlImage: "", isFriend: false),
-        FakeFriend(id: "2", name: "Andres el come hombre", urlImage: "", isFriend: false),
-        FakeFriend(id: "3", name: "Layla", urlImage: "", isFriend: false),
-        FakeFriend(id: "4", name: "Tu vieja", urlImage: "", isFriend: false)
-    ]
+    @State private var fullFakeFriends: [FakeFriend] = []
     
     var body: some View {
         NavigationView(){
-            List(fullFakeFriends) { friend in
-                NavigationLink(destination: { Text(friend.name)}) {
-                    Text("\(friend.id)   \(friend.name)")
+            
+            if showInitialMessage {
+                Text("Type a least 3 characters and \n press enter to perform a \n search of people to add as friends.")
+                    .fontWeight(.black)
+                    .multilineTextAlignment(.center)
+                
+            }else {
+                List(fullFakeFriends) { friend in
+                    NavigationLink(destination: { Text(friend.name)}) {
+                        Text("\(friend.id.lowercased())   \(friend.name.lowercased())")
+                    }
+                }.overlay{
+                    if fullFakeFriends.count == 0 {
+                        Text("There are not people for this \n criteria. Try again with a \n different name.")
+                            .fontWeight(.black)
+                            .multilineTextAlignment(.center)
+                    }
                 }
             }
         }.searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search friends")
+            .overlay{
+                Button(action: {
+                    performSearch("")
+                }){Text("LIST") }
+            }
         
+    }
+    
+    private func performSearch(_ term: String) {
+        fullFakeFriends = [
+//        FakeFriend(id: "1", name: "Gian El hombre", urlImage: "", isFriend: false),
+//        FakeFriend(id: "2", name: "Andres el come hombre", urlImage: "", isFriend: false),
+//        FakeFriend(id: "3", name: "Layla", urlImage: "", isFriend: false),
+//        FakeFriend(id: "4", name: "Tu vieja", urlImage: "", isFriend: false)
+        ]
+        showInitialMessage = false
     }
 }
 
