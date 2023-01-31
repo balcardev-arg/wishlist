07a3c9621d56caa82320af57e3fb32be0eff5afc
layla <cisneroslay@hotmail.com>
2023-01-31 13:15:18 -0300

    ConfigurationURL

---
+++ WishList/WishList/Configuration.swift
@@ -0,0 +1,12 @@
+//
+//  Configuration.swift
+//  WishList
+//
+//  Created by Layla Cisneros on 27/01/2023.
+//
+
+import Foundation
+
+struct Configuration {
+    static let baseUrl = "https://us-central1-balcardev-wishlist.cloudfunctions.net/app/api"
+}
