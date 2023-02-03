2d7f0692c0f86b2f368423e461d4e938af9031c3
layla <cisneroslay@hotmail.com>
2023-02-03 16:10:46 -0300

    Remove unused firebase configuration file.

--- WishList/WishList/WishListApp.swift
+++ WishList/WishList/WishListApp.swift
@@ -8,15 +8,8 @@
 import SwiftUI
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        FirebaseApp.configure()
-    }
-}
-
 @main
 struct WishListApp: App {
-    @UIApplicationDelegateAdaptor (AppDelegate.self) var delegate
     
     @StateObject var credentialsManager = CredentialsManager()
     
